3e85dbad2ab63502bf6dc50aef14cb9d848356d3
IliaAlemasov <lastsunset666@gmail.com>
2024-02-13 14:05:37 +0700

    +lego

---
+++ lego.py
@@ -0,0 +1,7 @@
+from pybricks.hubs import EV3Brick, MoveHub
+
+#ev3 = EV3Brick.__init__()
+
+#move = MoveHub.__init__()
+
+
--- vera.py
+++ vera.py
@@ -2,3 +2,9 @@ from soud_engine_settings import settings_sound
 import pyttsx3 as pt
 
 
+
+
+
+
+
+
